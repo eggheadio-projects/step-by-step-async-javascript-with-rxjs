58141e7636d013aa0acae6244f603c3fa95c62e8
creeland <cree@provinsal.com>
2017-09-05 13:01:45 -0700

    Add lesson 2 example code

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ const Observable = Rx.Observable;
 
 
 // App logic
-console.log("hello world!")
+const startButton = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
+
+const start$ = Observable.fromEvent(startButton, 'click');
+const interval$ = Observable.interval(1000);
+const stop$ = Observable.fromEvent(stopButton, 'click');
+
+const intervalThatStops$ = interval$
+    .takeUntil(stop$);
+
+start$
+    .switchMapTo(intervalThatStops$)
+    .subscribe((x)=> console.log(x));
+
+
