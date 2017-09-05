c2c0e186317a5ee21d9406e4ce3459511223d11a
creeland <cree@provinsal.com>
2017-09-05 11:09:04 -0700

    Add example from lesson

---
+++ index.js
@@ -0,0 +1,19 @@
+import Rx from 'rxjs'
+const Observable = Rx.Observable;
+
+
+// App logic
+const startButton = document.querySelector('#start');
+
+const start$ = Observable.fromEvent(startButton, 'click');
+const interval$ = Observable.interval(1000);
+
+const startInterval$ = start$
+    .switchMapTo(interval$);
+
+
+startInterval$
+    .subscribe((x)=> console.log(x));
+
+
+
