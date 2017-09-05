f1ba3e3031da68bb6234d1b52737acd71768a0ad
creeland <cree@provinsal.com>
2017-09-05 15:18:23 -0700

    Add Lesson 06 Example Code

--- index.js
+++ index.js
@@ -5,23 +5,28 @@ const Observable = Rx.Observable;
 // App logic
 const startButton = document.querySelector('#start');
 const stopButton = document.querySelector('#stop');
+const resetButton = document.querySelector('#reset');
 
 const start$ = Observable.fromEvent(startButton, 'click');
 const interval$ = Observable.interval(1000);
 const stop$ = Observable.fromEvent(stopButton, 'click');
+const reset$ = Observable.fromEvent(resetButton, 'click');
 
-const intervalThatStops$ = interval$
-    .takeUntil(stop$);
 
 const data = {count:0};
 const inc = (acc)=> ({count: acc.count + 1});
 const reset = (acc)=> data;
 
+const intervalThatStops$ = interval$
+    .takeUntil(stop$);
+
+const incOrReset$ = Observable.merge(
+    intervalThatStops$.mapTo(inc),
+    reset$.mapTo(reset)
+);
+
 start$
-    .switchMapTo(intervalThatStops$)
-    .mapTo(inc)
+    .switchMapTo(incOrReset$)
     .startWith(data)
-    .scan((acc, curr)=> {
-      return curr(acc)
-     })
+    .scan((acc, curr)=> curr(acc))
     .subscribe((x)=> console.log(x));
