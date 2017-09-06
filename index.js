4be4066cee2b3b92eb541f6f2cb9a13b816e65f0
creeland <cree@provinsal.com>
2017-09-06 08:47:37 -0700

    Add Lesson 11 Example Code

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ Observable.combineLatest(
     input$,
     (timer, input)=> ({count: timer.count, text: input})
 )
+    .takeWhile((data)=> data.count <= 3)
     .filter((data)=> data.count === parseInt(data.text))
     .subscribe(
         (x)=> console.log(x),
